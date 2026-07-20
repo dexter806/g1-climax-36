7482cc6c56b52b5767a1e570cd0aa099b2d93648
dexter806 <dexter806@gmail.com>
2026-07-20 13:39:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -270,6 +270,7 @@ const OUT_WRESTLERS = ["Shota Umino"];
    ============================================ */
 const C_BLOCK_WINS = [
   { name: "Hartley Jackson", night: "N2 \u00b7 Jul 18" },
+  { name: "Hartley Jackson", night: "N3 \u00b7 Jul 19" }
 ];
 
 /* Generates a simple original avatar for each wrestler: a silhouette
